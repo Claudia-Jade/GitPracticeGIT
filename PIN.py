524d321e6ca89041823f4b7902c745d2c344d4c6
Claudia Jade Fayne <CJ@CJs-MacBook-Pro.local>
2022-02-02 15:28:39 +0000

    changed client input terminology

--- PIN.py
+++ PIN.py
@@ -4,12 +4,12 @@ import getpass
 attempt_count = int(3)
 # no. of total available attempts
 pin = '1234'
-# defne pininput as '1234' sting
+# define pininput as '1234' sting
 
-InputCount = getpass.getpass(prompt='Type your Pin: ')
+client_attempt = getpass.getpass(prompt='Type your Pin: ')
 # define the prompt for getpass
 
-while InputCount != pin:
+while client_attempt != pin:
     # attempt while loop
     attempt_count = attempt_count - 1
     # condition for while loop
@@ -17,9 +17,9 @@ while InputCount != pin:
         print('<', 'Attempts left: ', attempt_count, 'of 3.', 'Too many attempts. Account locked.', '>')
         break
     print('<', 'Attempt No.', attempt_count, 'of 3.', 'Incorrect PIN.', '>')
-    InputCount = getpass.getpass('Type your PIN: ')
+    client_attempt = getpass.getpass('Type your PIN: ')
 #     getpass hides the password in this stage of the code
 
-if InputCount == pin:
+if client_attempt == pin:
     print('<', 'Correct Pin.', '>')
 # if statement outside of while loop
